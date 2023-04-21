724c04727c61cd487683177cfb074aec1ae45ba5
mrpizza-lisboa <mrpizzacentral@gmail.com>
2023-04-21 10:49:16 +0100

    v2.0.0 - dev-master More Organized

---
+++ src/UI/messages/pt-PT/yii2fullcalendar.php
@@ -0,0 +1,27 @@
+<?php
+/*
+ * @author Ricardo Grangeia Dias <ricardograngeia@gmail.com>
+ * @profile PHP Developer
+ */
+
+return [
+	'Today' => 'Hoje',
+	'Month' => 'Mês',
+	'Week' => 'Semana',
+	'Day' => 'Dia',
+	'List' => 'Lista',
+	'Year' => 'Ano',
+	'Add' => 'Criar',
+	'Draggable Events' => 'Eventos Arrastáveis',
+	'Remove after drop' => 'Remover após soltar',
+	'Work on UI design' => 'Trabalhe no design da interface do usuário',
+	'Sleep tight' => 'Dormir Bem',
+	'Do homework' => 'Fazer os trabalhos de casa',
+	'Create Event' => 'Criar Evento',
+	'Go home' => 'Ir para casa',
+	'Lunch' => 'Almoço',
+	'Event Title' => 'Título do Evento',
+	'Next' => 'Próximo',
+	'Previous' => 'Anterior',
+	'view' => 'vista',
+];
